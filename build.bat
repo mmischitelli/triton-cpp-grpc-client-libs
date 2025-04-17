f5306fed367965f4a76d5592166141ccfe0181e2
mmischitelli <michelemischitelli@outlook.com>
2025-04-17 06:53:18 +0200

    Add build.bat with instructions for building triton client

---
+++ build.bat
@@ -0,0 +1,7 @@
+cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE='c:/vcpkg/scripts/buildsystems/vcpkg
+.cmake' -DCMAKE_INSTALL_PREFIX=install -DTRITON_ENABLE_CC_GRPC=ON -DTRITON_COMMON_REPO_TAG='r25.04' -DTRITON_THIRD_PARTY
+_REPO_TAG='r25.04' -DTRITON_CORE_REPO_TAG='r25.04' -DTRITON_SERVER='2.55.0' ../
+
+git config --global --add safe.directory '*'
+
+msbuild.exe cc-clients.vcxproj -p:Configuration=Release /m
